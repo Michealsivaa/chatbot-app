57a0ecdad219afbff4fb304bd68161d0965dcb3a
Maniooty <manigandan.mahalingam@pptservices.com>
2024-03-05 10:41:59 +0530

    Design changes

--- src/components/Login/logIn.jsx
+++ src/components/Login/logIn.jsx
@@ -9,7 +9,7 @@ const LogIn = () => {
   return (
     <>
       <Header />      
-        <Grid container spacing={2} sx={{ padding: 4, background:"#f3f4f6", mt: 0, mb: 2 }}>
+        <Grid container spacing={2} sx={{ padding: "20px 20px", background:"#f3f4f6", mt: 0, mb: 2 }}>
             <Grid item xs={12} lg={4} xl={12}>
             </Grid>
             <Grid item xs={12} lg={4} xl={12} sx={{ lineHeight: "30px", background: "#fff", color: "#000", padding: 2, borderRadius: 2 }}>
